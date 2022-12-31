7a15c1132440d7eb39b7e3c01caff4708a65b96b
nandohidayat <mnandohidayat@gmail.com>
2022-12-31 15:13:41 +0700

    done

--- src/main/java/com/nandohidayat/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/nandohidayat/springboot/learnspringboot/CourseController.java
@@ -1,10 +1,10 @@
 package com.nandohidayat.springboot.learnspringboot;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class CourseController {
